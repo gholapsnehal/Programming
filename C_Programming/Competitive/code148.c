2f4d8109cab1b30677a253c4f6166487d734db28
Snehal Gholap <snehalgholap3025@gmail.com>
2026-06-30 18:35:40 +0530

    added new files

---
+++ C_Programming/Competitive/code148.c
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name:   CheckBit
+// Input:           int
+// Output:          BOOL
+// Description:     Write a program which checks whether 15th bit is ON or OFF.
+// Date :           26/06/2026
+// Author:          Snehal Gholap
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL CheckBit(UINT iNo)
+{
+    UINT iMask = 16384;
+    UINT iAns = 0;
+
+    iAns = iNo & iMask;
+
+    if(iAns == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : \n");
+    scanf("%u",&iValue);
+
+    bRet =CheckBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("Bit is ON\n");
+    }
+    else
+    {
+        printf("Bit is OFF\n");
+    }
+
+
+    return 0;
+}
---
+++ C_Programming/Competitive/code149.c
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name:   CheckBit
+// Input:           int
+// Output:          BOOL
+// Description:     Write a program which checks whether 5th & 18th bit is ON or OFF.
+// Date :           27/06/2026
+// Author:          Snehal Gholap
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL CheckBit(UINT iNo)
+{
+    UINT iMask = (1 << 6) | (1 << 14) | (1 << 20) | (1 << 27);
+    UINT iAns = 0;
+
+    iAns = iNo & iMask;
+
+    if(iAns == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : \n");
+    scanf("%u",&iValue);
+
+    bRet =CheckBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("Bit is ON\n");
+    }
+    else
+    {
+        printf("Bit is OFF\n");
+    }
+
+
+    return 0;
+}
