26e941685bcc8aeec2a758deb6802fad61561514
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-19 23:19:40 +0530

    Directory traversal

---
+++ Python_Programming/Practice/directoryTraversal2.py
@@ -0,0 +1,16 @@
+import os
+
+def main():
+
+    for FolderName, SubFolder, FileName in os.walk("Marvellous"):
+        print("FolderName : ",FolderName)
+
+        for subf in SubFolder:
+            print("SubFolder Name :",subf)
+
+        
+
+
+
+if __name__ == "__main__":
+    main()
