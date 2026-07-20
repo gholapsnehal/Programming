3eb21e543a3d859343e425b2f766931df39d1aa8
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-20 13:47:00 +0530

    While Iteration

---
+++ Data_Structure/C/IterationWhileLoop.c
@@ -0,0 +1,23 @@
+
+// Iteration using while loop
+#include<stdio.h>
+
+void Display()
+{
+    auto int i = 0;
+    i = 1;        // this i variable is auto 
+
+    while(i <= 4)
+    {
+        printf("Jay Ganesh\n");
+        i++;
+    } 
+        
+}
+
+int main()
+{
+    Display();
+    
+    return 0;
+}
