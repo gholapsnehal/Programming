a7439a8a47bc69cbd9102a27bd7f0be8c3b9e644
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-20 13:45:25 +0530

    While Iteration

---
+++ Data_Structure/C/IterationWhile1.c
@@ -0,0 +1,28 @@
+//while loop
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    auto int i = 0;
+    i = 1;       
+
+    while(i <= iNo)
+    {
+        printf("Jay Ganesh\n");
+        i++;
+    } 
+        
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter frequency : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    
+    return 0;
+}
