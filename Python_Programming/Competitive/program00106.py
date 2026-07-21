7b856ac6959ced45fb64069f725ca18cc558025a
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-21 14:57:16 +0530

    Display content of a file

---
+++ Python_Programming/Competitive/program00106.py
@@ -0,0 +1,56 @@
+##################################################################################################
+#
+# Function Name : DisplayFile
+# Input         : String
+# Output        : None
+# Description   : Accept file name from user and display the contents of the
+#                 file on the console
+# Date          : 18/07/2026
+# Author        : Snehal Gholap
+#
+##################################################################################################
+
+import os
+
+def DisplayFile(FileName):
+
+    try:
+
+        fobj = open(FileName, "r")
+
+        Data = fobj.read()
+
+        print(Data)
+
+        fobj.close()
+
+    except FileNotFoundError:
+        print("Unable to open file as file does not exist")
+        return
+
+##################################################################################################
+#
+# Function Name : main
+# Input         : None
+# Output        : None
+# Description   : Accept file name from user and display the contents of the
+#                 file
+# Date          : 18/07/2026
+# Author        : Snehal Gholap
+#
+##################################################################################################
+
+def main():
+
+    FileName = input("Enter file name : ")
+
+    DisplayFile(FileName)
+
+##################################################################################################
+#
+# Starter
+#
+##################################################################################################
+
+if __name__ == "__main__":
+    main()
