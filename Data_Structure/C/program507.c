289379318e4ef358ab015d38cea41ac46cd93472
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-23 14:12:36 +0530

    Recursion practice

---
+++ Data_Structure/C/program507.c
@@ -0,0 +1,24 @@
+
+
+#include<stdio.h>
+
+void Display()
+{
+    auto int i = 0;
+     
+    // infinite loop : itwill just iterate infinite
+    while(1)
+    {
+        printf("Jay Ganesh...%d \n",i); 
+        i++;
+    } 
+   
+        
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
