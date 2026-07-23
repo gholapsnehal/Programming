f16bd69e871a79f9c049336516da66bd612dc303
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-23 14:06:35 +0530

    Recursion practice

---
+++ Data_Structure/C/program501.c
@@ -0,0 +1,24 @@
+//RECURSION
+
+// Iteration using while loop
+#include<stdio.h>
+
+void Display()
+{
+    auto int i = 0;
+    i = 1;        // this i variable is auto 
+
+    while(i <= 4)
+    {
+        printf("Jay Ganesh\n");
+        i++;
+    } 
+        
+}
+
+int main()
+{
+    Display();
+    
+    return 0;
+}
