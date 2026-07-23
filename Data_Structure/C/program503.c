b04c767af7cadb083081014b1cce40beb04f9262
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-23 14:07:17 +0530

    Recursion practice

---
+++ Data_Structure/C/program503.c
@@ -0,0 +1,19 @@
+//RECURSION
+
+#include<stdio.h>
+
+void Display()
+{
+    printf("Jay Ganesh\n");    
+        
+}
+
+int main()
+{
+    Display();
+    Display();
+    Display();
+    Display();
+    
+    return 0;
+}
