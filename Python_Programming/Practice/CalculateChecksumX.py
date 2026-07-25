527d7a4d3e47c79e03031918142c53881b1e524c
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-25 22:17:16 +0530

    Practice code 2

---
+++ Python_Programming/Practice/CalculateChecksumX.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import hashlib      # checksum check krnyache function
+
+
+def CalCheckSum(Filename):
+    fobj = open(Filename,"rb")     # read in binary format Binary IO
+
+    hobj = hashlib.md5()           # 
+
+    #list = buffer
+    Buffer = fobj.read(1000)      # reading 1000 bytes at one time till file data ends
+
+    while(len(Buffer) > 0):
+        hobj.update(Buffer)
+        Buffer = fobj.read(1000)
+
+    fobj.close()
+
+    return hobj.hexdigest()   # checksum
+
+
+def main():
+    
+    Ret = CalCheckSum("DemoX.txt")
+
+    print("checksum of file is : ",Ret)
+   
+ 
+if __name__ == "__main__":
+    main()
+
+
