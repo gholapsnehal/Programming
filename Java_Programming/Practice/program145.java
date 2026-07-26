67edc08b270bed1f6af44111d9f708b654f0c77e
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-26 23:49:17 +0530

    Java practice code

---
+++ Java_Programming/Practice/program145.java
@@ -0,0 +1,39 @@
+
+//import java.util.*;
+
+class program145
+{
+    public static void Update(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        Arr[iCnt]++;
+    }
+
+}
+    public static void main(String A[])
+    {
+
+    int Brr[] = {10,20,30,40,50};
+    int iCnt = 0;
+
+    System.out.println("Array elements before function call: ");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        System.out.println(Brr[iCnt]);
+    }
+
+    Update(Brr,5);
+
+    System.out.println("Array elements after function call: ");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        System.out.println(Brr[iCnt]);
+    }
+}
+
+}
