b3622cf61c5f89658ad81d53863d89a4996b4897
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-26 23:46:56 +0530

    Java practice

---
+++ Java_Programming/Practice/program140.java
@@ -0,0 +1,22 @@
+//JAVA: 
+import java.util.*;
+
+
+class program140
+{
+    public static void main(String A[])
+    {
+        //Scannar sobj = new Scannar(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+    
+        // not a good programming practice
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+
+        System.out.println(iCnt); // not a good programming practice
+
+    }
+}
