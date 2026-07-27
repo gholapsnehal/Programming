aafc8b83c0a96b0ba44a0fcdcb2b66f3e85b01d2
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-27 17:42:07 +0530

    Recursion assignment practice

---
+++ C_Programming/Competitive/code184.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static char ch = 'A';
+  
+    if(iNo > 0)
+    {       
+        printf("%c\t",ch);
+        ch++;
+
+        Display(iNo - 1);
+    }    
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+
+
+    return 0;
+}
