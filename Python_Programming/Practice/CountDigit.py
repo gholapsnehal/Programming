55c6ef8538eaa01b7abc6c9a368abf730bf2119d
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-28 20:11:35 +0530

    count of digits in a number

---
+++ Python_Programming/Practice/CountDigit.py
@@ -0,0 +1,24 @@
+def Count(No):
+
+    Count = 0
+    Digit = 0
+
+    while(No > 0):
+        Digit = No % 10
+        Count = Count + 1
+        No = No // 10
+
+    return Count
+
+
+def main():
+
+    Value = int(input("Enter number : "))
+
+    Ret = Count(Value)
+
+    print(f"Count of digits in {Value} is {Ret}")
+
+
+if __name__ == "__main__":
+    main()
