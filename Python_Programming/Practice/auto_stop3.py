5e532b038d3095ccb9007832d28d77810874cc04
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-30 20:20:45 +0530

    auto stop scheduler practice

---
+++ Python_Programming/Practice/auto_stop3.py
@@ -0,0 +1,49 @@
+import schedule
+import time
+
+def Display(fobj):
+
+    try:
+
+        fobj.write("Jay Ganesh...\n")
+
+    except ValueError as eobj:
+        print("Unable to write into file : ",eobj)
+
+
+
+def ExecuteTask(counter,fobj):
+
+    try:
+        Display(fobj)
+        counter[0] = counter[0] + 1
+        print("counter : ",counter[0])
+
+    except Exception as e:
+        print("unable to execute task : ",e)
+
+
+
+def main():
+
+    counter = [0]
+
+    try:
+
+        fobj = open("Alog.txt","w")
+
+        print("script running...")
+        schedule.every(5).seconds.do(ExecuteTask,counter,fobj)
+
+        while(counter[0] < 5):
+            schedule.run_pending()
+            time.sleep(1)
+
+        fobj.close()
+
+    except Exception as e:
+        print("Error : ",e)
+
+
+if __name__ == "__main__":
+    main()
