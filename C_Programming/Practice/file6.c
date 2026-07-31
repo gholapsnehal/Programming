9af0b3e421400adda2b21b0c50fa138479be1fe1
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-31 22:44:59 +0530

    file practice

---
+++ C_Programming/Practice/file6.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<fcntl.h> 
+#include<unistd.h> // only linux based os 
+
+// write data in a file
+
+int main()
+{
+    int fd = 0;  
+    int iRet = 0;
+
+    fd = open("Marvellous.txt",O_RDWR);    // O_RDWR - read and write
+
+    if(fd == -1)
+    {
+        printf("Unable to open a file\n");
+    }
+    else
+    {
+        printf("File gets successfully opened with fd : %d\n",fd);
+
+        iRet = write(fd,"Jay Ganesh...",13);    
+
+        printf("%d bytes gets successfully written \n",iRet); 
+
+        close(fd);
+    }
+
+
+    return 0;
+}
+
+// man close- linux command 
+// close system call
+
