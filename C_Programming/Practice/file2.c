89cd775478f8bac160e067f0745fe3ce2095c6e5
Snehal Gholap <snehalgholap3025@gmail.com>
2026-07-31 22:42:01 +0530

    file practice

---
+++ C_Programming/Practice/file2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<fcntl.h> 
+
+int main()
+{
+    int fd = 0;  
+
+    fd = creat("Marvellous.txt",0777);
+
+    if(fd == -1)
+    {
+        printf("Unable to create a file\n");
+    }
+    else
+    {
+        printf("File gets successfully created with fd : %d\n",fd);
+    }
+
+
+    return 0;
+}
+
