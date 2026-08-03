9d0ac9fd33050d95d229daaec707e8c4a5188d37
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-04 00:13:08 +0530

    Practice code

---
+++ JavaScript_Programming/Competitive/demo2.js
@@ -0,0 +1,19 @@
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Description : Using Built in functionality
+//  Author      : Snehal Gholap
+//  Date        : 03/08/2026
+//
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+let No = 11;
+console.log("data type of No : ",typeof(No));
+
+let No1 = 11.20000;
+console.log("data type of NO1 : ",typeof(No1));
+
+let str = "Jay Ganesh";
+console.log("data type of str : ",typeof(str));
