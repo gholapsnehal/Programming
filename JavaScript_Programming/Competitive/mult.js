119c39413ba12f26032ccb29aa864bd40e14c9b1
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-04 00:14:12 +0530

    Practice code

---
+++ JavaScript_Programming/Competitive/mult.js
@@ -0,0 +1,21 @@
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Description : Multiplication of Two Numbers
+//  Author      : Snehal Gholap
+//  Date        : 03/08/2026
+//
+/////////////////////////////////////////////////////////////////////////////
+
+
+function Mult(No1, No2)
+{
+    return No1 * No2
+}
+
+let value1 = 120
+let value2 = 2
+
+let Result = Mult(120,2)
+
+console.log("Multiplication of",value1,"and",value2,"is",Result)
