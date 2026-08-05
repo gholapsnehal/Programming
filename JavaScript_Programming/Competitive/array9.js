ad189c05b72307cd4833663b36fc33116985a5ef
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-05 20:39:41 +0530

    array practice

---
+++ JavaScript_Programming/Competitive/array9.js
@@ -0,0 +1,6 @@
+// includes() -> to check any value present or not in array
+// It will return boolean value true or false
+
+const fruits = ["apple","banana","orange","melon","kiwi"]
+
+console.log(fruits.includes("mango"))
