302b0e615e37a596491cf5b85de3f09f8a6b9dd8
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-05 20:50:46 +0530

    array practice

---
+++ JavaScript_Programming/Competitive/array11.js
@@ -0,0 +1,9 @@
+// splice() : basically make changes in an array
+
+const arr = [10,20,30,40,50]
+
+// 2 : index of value that want to delete
+// 1 : delete count
+arr.splice(2,1)
+
+console.log(arr)
