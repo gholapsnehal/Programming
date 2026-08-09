7eb6561438633893f91f1d7ce47ef0055da4eeff
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-09 19:26:40 +0530

    String Operation

---
+++ Java_Programming/Practice/String11.java
@@ -0,0 +1,16 @@
+// practice code for how we can check methods on string
+
+class program700
+{
+    public static void main(String A[])
+    {
+        String Header = "marvellous ";
+
+        System.out.println(Header);
+
+        Header.replaceAll("l","L");
+
+        System.out.println(Header);
+        
+    }
+}    
