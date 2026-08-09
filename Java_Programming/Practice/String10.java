f752a746f6f7b413968ceebce0251757e46756c1
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-09 19:19:07 +0530

    String Operation

---
+++ Java_Programming/Practice/String10.java
@@ -0,0 +1,20 @@
+// STRING CODE DIFF
+// header set kasa karacyha
+
+class program719
+{
+    public static void main(String A[])
+    {
+        // added white spaces
+        String Header = "a.txt 10";
+
+        System.out.println("Lenght : "+Header.length());
+
+        Header = Header + " ";
+        Header = Header + " ";
+        Header = Header + " ";
+
+        System.out.println("after adding white spaces : "+Header.length());
+        
+    }
+}    
