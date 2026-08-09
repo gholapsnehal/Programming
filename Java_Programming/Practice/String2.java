7b21816d9819810ba39f1472dce16b3e6c4fc1f8
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-09 19:12:17 +0530

    String Operation

---
+++ Java_Programming/Practice/String2.java
@@ -0,0 +1,14 @@
+
+class program710
+{
+    public static void main(String A[])
+    {
+        String Header = "india  is  my  country";
+
+        String Arr[] = Header.split(" ");
+
+        System.out.println(Header);
+        System.out.println("Number of words are in Header : "+Arr.length);
+
+    }
+}    
