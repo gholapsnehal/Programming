4c59a9e32e40672126a2ac1ea3240356f4dc5724
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-09 19:27:12 +0530

    String Operation

---
+++ Java_Programming/Practice/String12.java
@@ -0,0 +1,15 @@
+
+class String12
+{
+    public static void main(String A[])
+    {
+        String Header = "marvellous ";
+
+        System.out.println(Header);
+
+        Header = Header.replaceAll("l","L");
+
+        System.out.println("after replaceall : "+Header);
+        
+    }
+}    
