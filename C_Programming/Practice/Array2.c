4c35daaaa2080f850eb916fc59fa2544edb4fd31
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-10 15:51:16 +0530

    array practice

---
+++ C_Programming/Practice/Array2.c
@@ -0,0 +1,44 @@
+// ARRAY Defining size : no of elements : addition of numbers in array:
+
+#include<stdio.h>
+
+int Summation(int Arr[], int iSize)          
+{
+    int iSum = 0;
+    int iCnt = 0;
+     
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return iSum;
+     
+}
+
+
+int main()
+{
+    int iLength = 4;
+    int iCnt = 0;
+    int iRet = 0;
+
+    int Brr[iLength];           
+
+    printf("Enter the elements of an array: \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Summation(Brr,iLength);   
+    
+    printf("Sum of elements of array: %d\n",iRet);
+
+    return 0;
+}
+
+
+
