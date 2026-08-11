afb36fb4790e8b39ae1e56cb5f376c1b64598264
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-11 21:45:10 +0530

    File handling operation

---
+++ Java_Programming/Practice/FH7.java
@@ -0,0 +1,33 @@
+// directory traversal
+
+import java.io.*;
+import java.util.*;
+
+class FH7
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String DirectoryName = null;
+
+        System.out.println("Enter directory name : ");
+        DirectoryName = sobj.nextLine();
+
+        File fobj = new File(DirectoryName);
+
+        // to check directory present or not
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            System.out.println("Directory exists");
+
+            File fArr[] = fobj.listFiles();  // data mde files cha aaray bnla
+
+            System.out.println("Number of files in Data Directory : "+fArr.length);
+        }
+        else
+        {
+            System.out.println("There is no such directory");
+        }
+    }
+}    
