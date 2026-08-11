4681433efe4ef3c3f9ba2b1323bd9004a293bbc6
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-11 21:42:13 +0530

    File handling operation

---
+++ Java_Programming/Practice/FH2.java
@@ -0,0 +1,49 @@
+// FileOutputStrem : write()
+// FileInputStream : read()
+import java.io.*;
+import java.util.*;
+
+
+class FH2
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String Fname = null;
+        File fobj = null;
+        FileInputStream fiobj = null;
+        int iRet = 0;
+
+        System.out.println("Enter file name : ");
+        Fname = sobj.nextLine();
+
+        fobj = new File(Fname);
+
+        fiobj = new FileInputStream(fobj);   
+
+        //String Data = "Marvellous Infosystems";
+
+        byte Buffer[] = new byte[50];
+      
+        if(fobj.exists())
+        {
+            String str = null;
+
+            while((iRet = fiobj.read(Buffer)) != -1)
+            {
+                str = new String(Buffer);
+
+                System.out.print(str);
+
+                str = null;
+            }
+      
+        }
+
+        else
+        {
+            System.out.println("There is no such file");
+        }
+    }
+}
