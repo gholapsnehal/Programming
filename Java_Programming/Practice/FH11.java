b6f78a841744b2a6ea43949cc65b64acae3bf8c7
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-11 22:04:32 +0530

    File handling operation

---
+++ Java_Programming/Practice/FH11.java
@@ -0,0 +1,14 @@
+//import java.util.*;
+import java.io.*;
+
+
+class FH11
+{
+    public static void main(String A[]) throws IOException   // worst to do like this
+    {
+        File fobj = new File("Demo.txt");
+
+        fobj.createNewFile();      
+
+    }
+}
