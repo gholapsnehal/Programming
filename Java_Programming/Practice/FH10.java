0c4fcf56d1dc6e04fbe2aaecca8e1051e955879a
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-11 22:02:15 +0530

    File handling operation

---
+++ Java_Programming/Practice/FH10.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+import java.io.*;
+
+
+class FH10
+{
+    public static void main(String A[]) 
+    {
+        File fobj = new File("Demo.txt");
+
+        fobj.createNewFile();        // have to exception handle 
+
+
+    }
+}
