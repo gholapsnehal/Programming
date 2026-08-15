6e2ecc3eba7d40267c4c017d36c42522f8d9a031
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-15 22:23:22 +0530

    Linear regression practice

---
+++ Python_Programming/Practice/LinearRegressionUserDefined1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# procedural
+
+def Predictor():
+
+    # 1. Load the data
+
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of Independent variables X : ",X)
+    print("Values of Dependent variables Y : ",Y)
+
+
+
+def main():
+
+    # function call
+    Predictor()
+
+
+if __name__ == "__main__":
+    main()
