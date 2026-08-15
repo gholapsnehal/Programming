2965615af5892367f39464ae667347cd0a85f22b
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-15 22:44:41 +0530

    HashMap practice

---
+++ Java_Programming/Practice/hashmap8.java
@@ -0,0 +1,26 @@
+// Q. accept string from user and display frequency of each letter
+// hashmap in java
+
+import java.util.*;
+
+public class hashmap8
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        HashMap <Character, Integer> hobj = new HashMap<Character, Integer>();
+
+        hobj.put('a', 1);
+        hobj.put('b', 1);
+        hobj.put('a', 2);
+        hobj.put('b', 2);
+
+        System.out.println(hobj.keySet());
+
+      
+       sobj.close();  
+
+    }
+    
+}
