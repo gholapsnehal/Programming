a3af85ac8088b52703b36eac8d8633632b05f900
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-16 23:00:58 +0530

    simple linear regression

---
+++ Python_Programming/Practice/SimpleLinearRegression2.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def main():
+
+    X = np.array([[1],[2],[3],[4],[5]])
+
+    Y = np.array([50,55,60,65,70])
+
+    model = LinearRegression()
+
+    model = model.fit(X,Y)   # x_train, y_train
+
+    print(model.predict([[6]]))   # x_test
+
+    print("Coefficient : ", model.coef_)
+
+    print("Intercept : ", model.intercept_)
+
+
+
+if __name__ == "__main__":
+    main()
