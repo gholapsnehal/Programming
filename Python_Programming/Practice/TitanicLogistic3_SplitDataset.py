245a407a9fbd46d827e86b4f97e62ee993c2e53e
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-16 23:11:02 +0530

    logistic regression classification

---
+++ Python_Programming/Practice/TitanicLogistic3_SplitDataset.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import numpy as np
+import joblib as jb
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# pipeline
+
+# Step 1: Load data
+
+#----------------------------------------------------------------------------------------
+#      Function Name : LoadData
+#      Description   : Load the data from csv
+#      Input         : Name of csv file
+#      Output        : Data Frame
+#      Author        : Snehal Gholap
+#      Date          : 16/08/2026
+#----------------------------------------------------------------------------------------
+
+def LoadData(filename):
+
+    df = pd.read_csv(filename)
+
+    print("dataset loaded successfully")
+    print(df.head())
+
+    return df
+
+
+# Step 2: Data Preprocessing
+
+#----------------------------------------------------------------------------------------
+#      Function Name : PreprocessData
+#      Description   : It performs data analysis
+#      Input         : dataframe
+#      Output        : updated dataframe
+#      Author        : Snehal Gholap
+#      Date          : 16/08/2026
+#----------------------------------------------------------------------------------------
+
+def PreprocessData(df):
+
+    df = df.drop([
+        "Passengerid",
+        "zero",
+        "name"
+    ],
+    errors = "ignore"
+    )
+
+    # handle misisng values
+
+    df["Age"] = df["Age"].fillna(df["Age"].median())
+
+    df["Fare"] = df["Fare"].fillna(df["Fare"].median())
+
+    df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
+
+
+# convert categorical to numeric data
+
+    df = pd.get_dummies(
+        df,
+        columns=["Embarked"],
+        drop_first= True,
+        dtype=int
+    )
+
+
+    print(df.head())
+
+    print("Data preprocessing completed")
+
+
+    return df
+
+###################################################################################
+
+  # Step 3: Split data
+
+#----------------------------------------------------------------------------------------
+#      Function Name : splitData
+#      Description   : It performs splitting activity
+#      Input         : dataframe
+#      Output        : 4 subsets for training and testing
+#      Author        : Snehal Gholap
+#      Date          : 16/08/2026
+#----------------------------------------------------------------------------------------
+
+def splitData(df):
+
+    X = df.drop("Survived", axis = 1)
+
+    Y = df["Survived"]
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X,
+        Y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    print("Dataset splitting completed successfully")
+
+    return X_train, X_test, Y_train, Y_test
+
+
+#----------------------------------------------------------------------------------------
+#      Function Name : main
+#      Description   : Entry Point function
+#      Input         : None
+#      Output        : None
+#      Author        : Snehal Gholap
+#      Date          : 16/08/2026
+#----------------------------------------------------------------------------------------
+
+   
+def main():
+
+    # setp 1:
+
+    df = LoadData("MarvellousTitanicDataset.csv")
+
+    # setp 2:
+
+    df = PreprocessData(df)
+
+    # step 3
+
+    X_train, X_test, Y_train, Y_test = splitData(df)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
