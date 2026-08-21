403850dc1aaf1635eb29edcc7048815f72e3dde7
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-21 23:16:07 +0530

    Practice code

---
+++ Java_Programming/Practice/program862.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class program862
+{
+    public static void main(String A[]) 
+    {
+        HashMap <Integer,String> hobj = new HashMap<Integer,String>();
+
+        hobj.put(20,"C programming");
+        hobj.put(10,"Java programming");
+        hobj.put(30,"C programming");
+        
+        System.out.println(hobj);
+    }  
+}      
+
+
