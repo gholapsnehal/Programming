5ae9e1424a19fdc926ac5f92248f0848ac33692b
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-21 23:17:23 +0530

    Practice code

---
+++ Java_Programming/Practice/program864.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class program864
+{
+    public static void main(String A[]) 
+    {
+        TreeMap <Integer,String> hobj = new TreeMap<Integer,String>();
+
+        hobj.put(20,"C programming");
+        hobj.put(10,"Java programming");
+        hobj.put(30,"C programming");
+        hobj.put(10,"Java programming");
+        hobj.put(30,"C programming");
+        
+        System.out.println(hobj);
+    }  
+}  
+
+
+
+
