c3fa98cdf877f811032dc7b22d5bf85673f1fc61
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-28 00:16:28 +0530

    advanced problem statement solved 22

---
+++ Java_Programming/Competitive/Question93.java
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Assignment 58 : Question 2
+//  Program Name : MatrixFrequencyCalculator
+//  Description  : Accept matrix and one number from user and return frequency of that number
+//
+//  Inputs:
+//
+//      Two-dimensional integer array Arr
+//      Number of rows iRow
+//      Number of columns iCol
+//      Integer iNo
+//
+//  Validation:
+//
+//      Matrix dimensions must be positive (iRow > 0, iCol > 0)
+//
+//  Author       : Snehal Gholap
+//  Date         : 26/08/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class MatrixOperations
+{
+    public int Frequency(int Arr[][], int iRow, int iCol, int iNo)
+    {
+        int iCount = 0;
+
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                if(Arr[i][j] == iNo)
+                {
+                    iCount++;
+                }
+            }
+        }
+
+        return iCount;
+    }
+}
+
+class Question93
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int iRow = sobj.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int iCol = sobj.nextInt();
+
+        if(iRow <= 0 || iCol <= 0)
+        {
+            System.out.println("Invalid dimensions. Rows and columns must be greater than 0");
+            return;
+        }
+
+        int Arr[][] = new int[iRow][iCol];
+
+        System.out.println("Enter matrix elements:");
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+
+        System.out.print("Enter the number to search: ");
+        int iNo = sobj.nextInt();
+
+        MatrixOperations mobj = new MatrixOperations();
+        int iRet = mobj.Frequency(Arr, iRow, iCol, iNo);
+
+        System.out.println("Output : " + iRet);
+    }
+}
