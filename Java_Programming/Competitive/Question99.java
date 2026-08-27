aa8a1a54a3c0c63097106a00803965a3d2a3a401
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-28 00:18:23 +0530

    advanced problem statement solved 28

---
+++ Java_Programming/Competitive/Question99.java
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Assignment 59 : Question 3
+//  Program Name : MatrixColumnReverser
+//  Description  : Accept matrix from user and reverse the contents of each column
+//
+//  Inputs:
+//
+//      Two-dimensional integer array Arr
+//      Number of rows iRow
+//      Number of columns iCol
+//
+//  Validation:
+//
+//      Matrix dimensions must be positive (iRow > 0, iCol > 0)
+//
+//  Author       : Snehal Gholap
+//  Date         : 26/08/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class MatrixOperations
+{
+    public void ReverseCol(int Arr[][], int iRow, int iCol)
+    {
+        for(int j = 0; j < iCol; j++)
+        {
+            int iStart = 0;
+            int iEnd = iRow - 1;
+
+            while(iStart < iEnd)
+            {
+                int temp = Arr[iStart][j];
+                Arr[iStart][j] = Arr[iEnd][j];
+                Arr[iEnd][j] = temp;
+
+                iStart++;
+                iEnd--;
+            }
+        }
+
+        // Display matrix after reversing each column
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Question99
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int iRow = sobj.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int iCol = sobj.nextInt();
+
+        if(iRow <= 0 || iCol <= 0)
+        {
+            System.out.println("Invalid dimensions. Rows and columns must be greater than 0");
+            return;
+        }
+
+        int Arr[][] = new int[iRow][iCol];
+
+        System.out.println("Enter matrix elements:");
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+
+        MatrixOperations mobj = new MatrixOperations();
+        System.out.println("Output :");
+        mobj.ReverseCol(Arr, iRow, iCol);
+    }
+}
