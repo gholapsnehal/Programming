065e72818832d95f119f6ae185e5cb43ffc9a6c1
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-28 00:16:08 +0530

    advanced problem statement solved 21

---
+++ Java_Programming/Competitive/Question92.java
@@ -0,0 +1,79 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Assignment 58 : Question 1
+//  Program Name : MatrixDiagonalAdder
+//  Description  : Accept matrix from user and return addition of primary diagonal elements
+//
+//  Inputs:
+//
+//      Two-dimensional integer array Arr
+//      Number of rows iRow
+//      Number of columns iCol
+//
+//  Validation:
+//
+//      Matrix dimensions must be positive (iRow > 0, iCol > 0)
+//
+//  Author       : Snehal Gholap
+//  Date         : 26/08/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class MatrixOperations
+{
+    public int AddDiagonal(int Arr[][], int iRow, int iCol)
+    {
+        int iSum = 0;
+
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                if(i == j)
+                {
+                    iSum += Arr[i][j];
+                }
+            }
+        }
+
+        return iSum;
+    }
+}
+
+class Question92
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int iRow = sobj.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int iCol = sobj.nextInt();
+
+        if(iRow <= 0 || iCol <= 0)
+        {
+            System.out.println("Invalid dimensions. Rows and columns must be greater than 0");
+            return;
+        }
+
+        int Arr[][] = new int[iRow][iCol];
+
+        System.out.println("Enter matrix elements:");
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+
+        MatrixOperations mobj = new MatrixOperations();
+        int iRet = mobj.AddDiagonal(Arr, iRow, iCol);
+
+        System.out.println("Output : " + iRet);
+    }
+}
