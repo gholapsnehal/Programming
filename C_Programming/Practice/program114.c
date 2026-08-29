ded3b4d97fe372ad6485d161c1c3af013ea14864
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-29 20:08:00 +0530

    C problem statement

---
+++ C_Programming/Practice/program114.c
@@ -0,0 +1,26 @@
+// ARRAY Defining size : no of elements
+
+#include<stdio.h>
+
+void Display(int Arr[], int iSize)          
+{
+
+    int iCnt = 0;
+                
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+     
+}
+
+
+int main()
+{
+    int Brr[4] = {10,20,30,40};
+
+    Display(Brr,4);               // call by address and value (Brr,4)  
+
+    return 0;
+}
+
