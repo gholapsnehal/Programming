da63f066d9eee7be392c0f0547feca9e5e1e0cd6
Snehal Gholap <snehalgholap3025@gmail.com>
2026-08-29 19:58:34 +0530

    C problem statement

---
+++ C_Programming/Practice/program101.c
@@ -0,0 +1,27 @@
+// ARRAY STATIC
+
+#include<stdio.h>
+
+int main()
+{
+    int Arr[5] = {0};
+    int iCnt = 0;
+
+    printf("Enter the elements:\n");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        scanf("%d",&Arr[iCnt]);
+    }
+
+    printf("Elements of an array are: \n");
+    
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+   
+    return 0;
+}
+
+// Time complexity : O(2n)
