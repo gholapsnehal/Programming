4a0f6a46eae217c391d4c4d57b245fb6b4c77896
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-01 20:45:57 +0530

    problem statement solved 7

---
+++ Java_Programming/Practice/program87.java
@@ -0,0 +1,48 @@
+// count even digits
+import java.util.*;
+
+class DigitX
+{
+    public int CountOddDigits(int iNo)   
+    {
+        int iCount = 0;
+        int iDigit = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            
+            if(iDigit % 2 != 0)
+            {
+                iCount++;
+            }
+
+            iNo = iNo/10;
+            
+        }
+
+        return iCount;
+
+        }
+}
+
+class program87
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        DigitX dobj = new DigitX();         // object creation
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.CountOddDigits(iValue);        // function call
+
+        System.out.println("Number of odd digits are: "+iRet);
+    }
+
+}
