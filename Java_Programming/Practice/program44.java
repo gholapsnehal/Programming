cb02c7218c0c448c2c770c5c05672560b4d9432f
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-01 20:37:45 +0530

    practice code

---
+++ Java_Programming/Practice/program44.java
@@ -0,0 +1,45 @@
+// Type 3
+
+
+import java.util.Scanner;
+
+class NumberX                     // user defined class so with X suffix so that inbuilt function and defined not face any conflict
+{
+    public static boolean CheckDivisible(int iNo)                 
+    {
+        if((iNo % 3 == 0) && (iNo % 5 == 0))
+       {
+          return true;
+       }
+       else
+       {
+          return false;
+       }
+    }
+}
+
+class program44
+{
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       int iValue = 0;
+       boolean bRet = false;   // false = 0
+
+       System.out.println("Enter number: ");
+       iValue = sobj.nextInt();
+
+       bRet = NumberX.CheckDivisible(iValue);    
+       
+       if(bRet == true)
+       {
+        System.out.println("Number is divisible by 3 and 5");
+       }
+       else
+       {
+        System.out.println("Number is not divisible by 3 and 5");
+       }
+
+    }
+}
