24abf739eeb9477e6fae4f3a9773a8ba83b334b0
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-01 20:43:52 +0530

    problem statement solved 1

---
+++ Java_Programming/Practice/program81.java
@@ -0,0 +1,39 @@
+// accept number from user and display on screen in reverse order
+
+import java.util.*;
+
+class DigitX
+{
+    public void DisplayDigit(int iNo)       //return type void function will not return anything.
+    {
+        int iDigit = 0;
+
+        for( ; iNo != 0; )              // for(;;)
+        {
+            iDigit = iNo % 10;        // to separate digit
+            System.out.println(iDigit);
+            iNo = iNo/10;  
+        }
+    
+        }
+}
+
+class program81
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        DigitX dobj = new DigitX();         // object creation
+
+        int iValue = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        dobj.DisplayDigit(iValue);        // function call
+
+
+    }
+
+}
