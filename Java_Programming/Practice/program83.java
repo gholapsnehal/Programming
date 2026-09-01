7d893b2a766a33f4de5a6c59cce88dc002e11096
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-01 20:44:27 +0530

    problem statement solved 3

---
+++ Java_Programming/Practice/program83.java
@@ -0,0 +1,43 @@
+// accept number from user and display on screen in reverse order
+import java.util.*;
+
+class DigitX
+{
+    public int CountDigits(int iNo)   
+    {
+        int iCount = 0;
+        int iDigit = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iCount++;
+            iNo = iNo / 10; 
+            
+        }
+
+        return iCount;
+
+        }
+}
+
+class program83
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        DigitX dobj = new DigitX();         // object creation
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.CountDigits(iValue);        // function call
+
+        System.out.println("Count of digits: "+iRet);
+    }
+
+}
