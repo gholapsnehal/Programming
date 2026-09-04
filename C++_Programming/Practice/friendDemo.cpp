aeee54c74c0ff9bee7203aac81db56fae47498b9
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-04 17:57:13 +0530

    friend function demo

---
+++ C++_Programming/Practice/friendDemo.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public: int i;
+    private: int j;
+    protected: int k;
+
+    public:
+        Demo()
+        {
+            i = 11;
+            j = 21;
+            k = 51;           
+        }
+
+};
+
+void fun()
+{
+    Demo dobj;
+
+    cout<<dobj.i<<endl;
+
+    // ERROR
+    cout<<dobj.j<<endl;
+    cout<<dobj.k<<endl;
+
+}
+
+int main()
+{
+
+    fun();
+
+    return 0;
+}
