71af1fc92e3eb1a54b3692a4d4f35b5fed26e311
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-06 21:46:20 +0530

    final method

---
+++ Java_Programming/Practice/final_method.java
@@ -0,0 +1,30 @@
+class Demo
+{
+   public void fun()
+   {
+    System.out.println("Inside Demo fun");
+   }
+
+    public final void gun()
+   {
+    System.out.println("Inside Demo gun");
+   }
+
+}
+
+class Hello extends Demo
+{
+    public void gun()
+    {
+        System.out.println("Inside Hellow gun");
+    }
+}
+
+class final_method
+{
+    public static void main(String A[])
+    {
+        Hello hobj = new Hello();    
+     
+    }
+}
