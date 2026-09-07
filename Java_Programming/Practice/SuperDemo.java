bd66b0a7eb3830901f2516c047427195fa5fab84
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-07 20:43:59 +0530

    super demo

---
+++ Java_Programming/Practice/SuperDemo.java
@@ -0,0 +1,50 @@
+class Base
+{
+    public int i;
+    
+
+    public Base(int no)    // parameterized constructor
+    {
+        System.out.println("Inside Base constructor");
+
+        this.i = no;   
+    }
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived extends Base
+{
+   public int i;
+   
+   public Derived()
+   {
+    System.out.println("Inside Derived constructor");
+
+    this.i = 21;
+
+   }
+
+   public void gun()
+   {
+    System.out.println("Inside gun of Derived");
+
+    System.out.println("Value of i from Derived: "+i);
+    System.out.println("Value of i from Base: "+super.i);
+    
+   }
+}
+
+class SuperDemo    
+{
+    public static void main(String A[])
+    {
+        Derived dobj = new Derived();  // ERROR
+
+        dobj.gun();
+    }
+
+}
