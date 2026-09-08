0404efb5131a6be90b21a632696ea1a060899bc1
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-08 15:06:44 +0530

    to string demo

---
+++ Java_Programming/Practice/ToStringDemo.java
@@ -0,0 +1,19 @@
+
+class Marvellous
+{}
+
+class ToStringDemo
+{
+    public static void main(String A[])
+    {
+        // object creation of class
+
+        Marvellous mobj = new Marvellous();
+
+        System.out.println(mobj.toString());
+           
+    }
+
+}
+
+// output: Marvellous@382db087
