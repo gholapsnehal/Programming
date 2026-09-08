3b69cc742ec6ddc6ab75a6fe77b331e0fbb8b4a8
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-08 15:08:38 +0530

    to string demo

---
+++ Java_Programming/Practice/ToStringDemoX.java
@@ -0,0 +1,25 @@
+
+class Marvellous
+{
+    public String toString()
+    {
+        // override method
+        return "Marvellous Pune";
+    }
+}
+
+class ToStringDemoX
+{
+    public static void main(String A[])
+    {
+        // object creation of class
+
+        Marvellous mobj = new Marvellous();
+
+        System.out.println(mobj.toString());
+           
+    }
+
+}
+
+// output: 
