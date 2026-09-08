f1b078656c43b750a17d764758f5b6db4e576217
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-08 14:49:39 +0530

    hash code demo

---
+++ Java_Programming/Practice/hashCodeDemo.java
@@ -0,0 +1,32 @@
+
+class Marvellous
+{
+
+}
+
+class Hello
+{
+
+}
+
+class hashCodeDemo
+{
+    public static void main(String A[])
+    {
+        // object creation of class
+
+        Marvellous mobj = new Marvellous();
+        Hello hobj = new Hello();
+
+        System.out.println(mobj.hashCode());
+        System.out.println(hobj.hashCode());
+
+    }
+
+}
+
+/*
+1822383117
+233021551
+ */
+
