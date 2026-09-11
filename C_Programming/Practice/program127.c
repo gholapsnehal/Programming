ad8d071a83e3bd1fb98e5d412e886e0026ca47bc
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-12 00:23:18 +0530

    C codes practice

---
+++ C_Programming/Practice/program127.c
@@ -0,0 +1,61 @@
+// Program: Program to count frequency of 11.
+// Owner: Snehal Gholap
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountFrequency(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iCountFreq = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+       if(Arr[iCnt] == 11)
+       {
+         iCountFreq++;
+       }
+    }
+
+    return iCountFreq;
+
+}
+
+int main()
+{
+    int iLength = 0;
+    int *Brr = NULL;
+    int iCnt = 0;
+    int iRet = 0;
+
+    // enter number of elements
+
+    printf("Enter number of elements: ");
+    scanf("%d",&iLength);
+
+    // allocate memory
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    // accept values from user
+
+    printf("Enter the elements: \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    // use memory
+
+    iRet = CountFrequency(Brr,iLength);
+
+    printf("Frequency is : %d",iRet);
+
+    // deallocate memory
+
+    free(Brr);
+
+    return 0;
+}
