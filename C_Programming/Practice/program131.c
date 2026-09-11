53dca85d4cef84b5f0c9271ea99f3dffd4828fc5
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-12 00:24:27 +0530

    C codes practice

---
+++ C_Programming/Practice/program131.c
@@ -0,0 +1,85 @@
+// Program: searching : accept number from user linear search: check whether 11 is present or not.
+// without using flag : not a good approach but code will execute
+// Owner: Snehal Gholap
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// Time Complexity : O(N)
+bool LineaerSearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0;
+    int iCount = 0;
+      
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            iCount++;                                         
+        }   
+    }
+
+    if(iCount == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }      
+
+}
+
+int main()
+{
+    int iLength = 0;
+    int iValue = 0;
+    int iCnt = 0;
+    int *Brr = NULL;
+    bool bRet = false;
+
+    
+    // enter number of elements
+
+    printf("Enter number of elements: ");
+    scanf("%d",&iLength);
+
+    // allocate memory
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    // accept values from user
+
+    printf("Enter the elements: \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    // the element that you want to search
+
+    printf("Enter the element that you want to search: \n");
+    scanf("%d",&iValue);
+
+    // use memory
+
+    bRet = LineaerSearch(Brr,iLength,iValue);
+
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present");
+    }  
+
+    // deallocate memory
+
+    free(Brr);
+
+    return 0;
+}
