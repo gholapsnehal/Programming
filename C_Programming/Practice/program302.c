4ec65ef28fe4244f8f18b1fd8cefb68cbd2e7a26
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-11 23:11:45 +0530

    c basic codes

---
+++ C_Programming/Practice/program302.c
@@ -0,0 +1,24 @@
+// BITWISE OPERATOR
+
+/*
+Decimal : 197
+Octal : 305
+HexaDecimal : c5
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int No = 197;
+
+    printf("Decimal : %d\n",No);
+
+    printf("Octal : %o\n",No);
+
+    printf("HexaDecimal : %x\n",No);
+
+
+
+    return 0;
+}
