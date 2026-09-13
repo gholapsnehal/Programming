cad5f0d54390a63ce884ac91b18b96bff8d36f36
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-13 13:09:39 +0530

    cpp practices

---
+++ C++_Programming/Practice/program161.cpp
@@ -0,0 +1,43 @@
+// C++: OOP Designing
+
+#include<iostream>
+using namespace std;
+
+// its always good to write pragma in cpp code
+#pragma pack(1)
+class ArrayX
+{
+    public:
+    int *Arr;
+    int iSize;
+   
+    ArrayX(int X)
+    {
+       
+        iSize = X; 
+
+           
+        Arr = new int[iSize];
+
+    }
+
+    ~ArrayX()
+    {
+        
+        delete []Arr;
+    }
+};
+
+int main()
+
+{ 
+    ArrayX *aobj1 = new ArrayX(5);
+
+    // LOGIC (Function call)
+
+    delete aobj1;
+
+ 
+    return 0;
+ 
+}
