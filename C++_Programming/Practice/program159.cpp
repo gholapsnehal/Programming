8d0d607e6680204a351ce727da0e9f420cd4fe67
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-13 13:09:01 +0530

    cpp practices

---
+++ C++_Programming/Practice/program159.cpp
@@ -0,0 +1,49 @@
+// C++: OOP Designing
+
+#include<iostream>
+using namespace std;
+
+// its always good to write pragma in cpp code
+#pragma pack(1)
+class ArrayX
+{
+    public:
+    int *Arr;
+    int iSize;
+
+    
+    // parameterized constructor
+    ArrayX(int X)
+    {
+        cout<<"Inside constructor\n";
+
+        // character initialization
+        iSize = X; 
+
+        // Resource allocation      
+        Arr = new int[iSize];
+
+    }
+
+    // Destructor
+    ~ArrayX()
+    {
+        cout<<"Inside destructor\n";
+
+        // Resource deallocation
+        delete []Arr;
+    }
+};
+
+int main()
+
+{
+    // Static memory allocation for object aobj1
+    // ArrayX aobj1(5);   
+    
+    ArrayX *aobj1 = new ArrayX(5);
+   
+      
+    return 0;
+ 
+}
