18a7a1d25adace8c641ebbfbfd78ce2e66e644ec
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-14 16:11:50 +0530

    basic practices

---
+++ C_Programming/Practice/program325.c
@@ -0,0 +1,21 @@
+// range of integer in c and python
+
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iMask = 1; // it is 1 in hex: 0x1
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 33; iCnt++)   
+    {
+
+        printf("%d : %X\n",iCnt,iMask);    // X = hex                        
+        iMask = iMask << 1;
+    }
+
+
+    return 0;
+}
