277937b8be26ee9185544412022173586cf0f435
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-16 19:10:06 +0530

    c program practices

---
+++ C_Programming/Practice/program298.c
@@ -0,0 +1,29 @@
+//Swapping : duckCup whiteCup and thirdCup(temp)
+
+#include<stdio.h>
+
+// call by Address
+
+void Swap(int *ptr1, int *ptr2)
+{
+    int temp = 0;
+
+    temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+
+}
+
+int main()
+{
+    int i = 11;
+    int j = 21;
+
+    Swap(&i,&j);
+   
+    printf("%d\n",i);  
+    printf("%d\n",j); 
+
+
+    return 0;
+}
