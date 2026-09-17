fba41a0c847f9e9fb290c6db33a8709cd0c9e789
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-17 14:07:53 +0530

    c code practices

---
+++ C_Programming/Practice/program245.c
@@ -0,0 +1,30 @@
+// problems on string
+// how to pass string to function
+
+#include<stdio.h>
+
+void Display(char *str)
+{
+   printf("%c\n",*str);
+   str++;
+
+   printf("%c\n",*str);
+   str++;
+
+   printf("%c\n",*str);
+   str++;
+
+}
+
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+       
+    Display(Arr);
+  
+    return 0;
+}
