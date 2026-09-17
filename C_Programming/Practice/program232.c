917f0ed7a1df7fc1b810fffe915baf978402050a
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-17 13:58:24 +0530

    c code practices

---
+++ C_Programming/Practice/program232.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char str[] = {'J','a','y',' ','G','a','n','e','s','h','\0'};
+
+    printf("%s\n",str);  // print all character till \0
+
+    return 0;
+}
