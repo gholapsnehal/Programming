e71ebddcbebcd4f6e5d648fcf6950ce625b2d22b
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-17 13:58:06 +0530

    c code practices

---
+++ C_Programming/Practice/program231.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+    // it gives output with some garbage value
+    char str[] = {'J','a','y',' ','g','a','n','e','s','h'};
+
+    printf("%s\n",str);  // print all character till \0
+
+    return 0;
+}
