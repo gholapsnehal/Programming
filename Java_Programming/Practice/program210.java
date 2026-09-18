b5e345346f6b40f6a6130b805d3a2b6506543734
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-18 17:27:53 +0530

    java practice code

---
+++ Java_Programming/Practice/program210.java
@@ -0,0 +1,57 @@
+/*  36 times loop iterate
+iRow : 4
+iCol : 4
+
+@ @ @ $
+@ @ $ @
+@ $ @ @
+$ @ @ @
+ */
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        
+        for(i = iRow; i >= 1; i--)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == j)                // Diagonal
+                {
+                    System.out.print("$\t");
+                }
+                else 
+                {
+                    System.out.print("@\t");
+                }                
+            }  
+            System.out.println();         
+        }
+       
+    } 
+}
+
+public class program210
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        Pattern pobj = new Pattern();
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter number of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns :");
+        iValue2 = sobj.nextInt();
+
+        pobj.Display(iValue1,iValue2);
+
+    }
+    
+}
