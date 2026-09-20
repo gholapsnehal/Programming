fe54292f9252f128f9dc4b91b3398e586f059926
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-20 16:55:08 +0530

    Java practices

---
+++ Java_Programming/Practice/program260.java
@@ -0,0 +1,20 @@
+// problems on string in java
+import java.util.*;
+
+class program260
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String Arr = null;  // string class reference
+
+        System.out.println("Enter string : ");
+        Arr = sobj.nextLine();
+
+        System.out.println("Entered string : "+Arr);
+
+    }
+}
+
+
