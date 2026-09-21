98cedc0525797206dbcb90ab547f3831ad8e2c46
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-21 15:19:42 +0530

    java practice code

---
+++ Java_Programming/Practice/program777.java
@@ -0,0 +1,22 @@
+// Q. 
+// 
+
+import java.util.*;
+
+public class program777
+{
+    public static void main(String A[])
+    {
+        HashMap <String, Integer> hobj = new HashMap<String, Integer>();
+
+        hobj.put("PPA",1);
+        hobj.put("LB",1);
+        hobj.put("Python",1);
+        hobj.put("LSP",1);
+        hobj.put("LB",2);
+
+        System.out.println(hobj);
+        
+    }
+    
+}
