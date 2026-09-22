719f156601400d4cb903f37b840e7eb611eb98a8
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-22 19:03:38 +0530

    Addition Variable

---
+++ Python_Programming/Practice/AdditionVariable.py
@@ -0,0 +1,16 @@
+No1 = 0
+No2 = 0
+
+print("Enter first number :")
+No1 = input()
+
+print("Enter second number :")
+No2 = input()
+
+Ans = No1 + No2 
+
+print("Addition is : ",Ans)
+
+
+
+# by default it gives str for input
