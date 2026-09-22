390faa2332603a563533c1e1015d89e09121817d
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-22 19:02:00 +0530

    Addition Typecast

---
+++ Python_Programming/Practice/AdditionTypecast.py
@@ -0,0 +1,13 @@
+print("Enter first number :")
+No1 = input()
+
+print("Enter second number :")
+No2 = input()
+
+Ans = int(No1) + int(No2) 
+
+print("Addition is : ",Ans)
+
+
+
+# by default it gives str for input
