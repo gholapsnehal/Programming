56032e7ed41d37a09df0f9dbe450c833e3d8c373
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-22 18:38:50 +0530

    java practices

---
+++ Java_Programming/Practice/program796.java
@@ -0,0 +1,104 @@
+// Q. matrix
+//  OOP
+
+import java.util.*;
+
+class Matrix
+{
+    protected int Arr[][];
+    protected int iRow;
+    protected int iCol;
+    
+    public Matrix(int iRow, int iCol)
+    {
+        System.out.println("Inside matrix constructor");
+
+        this.iRow = iRow;
+        this.iCol = iCol;
+
+        Arr = new int[iRow][iCol];
+    }
+
+   /* protected void finalize()
+    {
+        System.out.println("Inside finalize method");
+        Arr = null;
+        System.gc();
+    }*/
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of matrix : ");
+
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+
+        }
+
+        sobj.close();
+    }
+
+    public void Display()
+    {
+        System.out.println("Elements of matrix are : ");
+
+        for(int i = 0; i < iRow; i++)
+        {
+            for(int j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j]+"\t");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
+
+class MatrixLB extends Matrix
+{
+    public MatrixLB(int iRow, int iCol)
+    {
+        super(iRow, iCol);
+    }
+
+}
+
+
+public class program796
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iRow = 0, iCol = 0;
+
+        System.out.println("Enter number of rows");
+        iRow = sobj.nextInt();
+
+        System.out.println("Enter number of columns");
+        iCol = sobj.nextInt();
+
+        MatrixLB mobj = new MatrixLB(iRow, iCol);
+
+        // function call
+
+        mobj.Accept();
+        mobj.Display();
+
+
+        sobj.close();
+
+        // now it will enter in finalize() method
+        mobj = null;
+        System.gc();            
+    }
+    
+}
