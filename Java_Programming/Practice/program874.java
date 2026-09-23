aa58c0def4b08df103ca37b9b037ee4b0a3411ad
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-23 16:55:55 +0530

    java code

---
+++ Java_Programming/Practice/program874.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+
+interface GetterSetter
+{
+    void Accept();
+    void Display();
+
+}
+
+
+class ArrayX implements GetterSetter
+{
+    protected int Arr[];
+    protected int iSize;
+
+    public ArrayX(int iSize)
+    {
+        this.iSize = iSize;
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of array");
+
+        for(int i = 0; i < this.iSize; i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+    }
+
+    public void Display()
+    {
+
+        System.out.println("Elements of  an array");
+
+        for(int i = 0; i < this.iSize; i++)
+        {
+            System.out.print(Arr[i]+"\t");
+        }
+
+        System.out.println();
+    }
+
+}
+
+// cannot be extended by othe classes
+final class Searching extends ArrayX
+{
+    public Searching(int iSize)
+    {
+        // It will call constructor of ArrayX class
+        super(iSize);    
+    }
+
+    public boolean LinearSearch(int iNo)
+    {
+        int i = 0;
+        boolean bFlag = false;
+
+        for(i = 0; i < super.iSize; i++)
+        {
+            if(Arr[i] == iNo)
+            {
+                bFlag = true;
+                break;
+            }
+
+        }
+
+        return bFlag;
+    }
+}
+
+
+public class program874
+{
+    public static void main(String A[])
+    {
+        Searching sobj = new Searching(5);
+
+        sobj.Accept();
+        sobj.Display();
+
+        if(sobj.LinearSearch(30))
+        {
+            System.out.println("Element is present in an array");
+        }
+        else
+        {
+            System.out.println("Element is not there in an array");
+        }
+    }
+}
+
+// worst case n
+// best case 1
