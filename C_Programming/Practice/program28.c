c8942efbc5678570bffeb988632bbb2655235d96
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-24 14:43:55 +0530

    C code

---
+++ C_Programming/Practice/program28.c
@@ -0,0 +1,27 @@
+// for loop iteration
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+     for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter the frequency : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    
+    return 0;
+}
