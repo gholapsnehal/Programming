1a9a1681283be9e672c847ab4cd2c2c3c09f3ffd
Snehal Gholap <snehalgholap3025@gmail.com>
2026-09-24 14:28:26 +0530

    C code

---
+++ C_Programming/Practice/program116.c
@@ -0,0 +1,34 @@
+// ARRAY Defining size : no of elements
+
+#include<stdio.h>
+
+void Display(int Arr[], int iSize)          
+{
+
+    int iCnt = 0;
+                
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+     
+}
+
+
+int main()
+{
+    int iLength = 4;
+
+    int Brr[iLength];           // member by member initialization technique
+
+    Brr[0] = 10;
+    Brr[1] = 20;
+    Brr[2] = 30;
+    Brr[3] = 40;
+
+
+    Display(Brr,iLength);           
+
+    return 0;
+}
+
